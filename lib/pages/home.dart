7956e8f96cd4508cf9ab18a25c7d4352d8d469c0
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2024-06-25 18:14:15 +0800

    Continuation of Training

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,33 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        color: Colors.blue.shade600,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: FlutterLogo(
+                      size: 40,
+                    ),
+                  ),
+                  Text("Sample Text Flutter"),
+                  Text("This is a subtext")
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
