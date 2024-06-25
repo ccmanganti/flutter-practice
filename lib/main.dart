650f24f42be60c0004b3ecb4c944ed5e31978f18
Christopher Clark Manganti <cpe.christopherclarkcmanganti@gmail.com>
2024-06-25 18:53:26 +0800

    Structured the Directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    const primaryColor = Color.fromARGB(255, 59, 149, 177);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Poppins', primaryColor: primaryColor, scaffoldBackgroundColor: Colors.white),
-      home: const Homepage(),
-    );
-  }
-}
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     const primaryColor = Color.fromARGB(255, 59, 149, 177);
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(fontFamily: 'Poppins', primaryColor: primaryColor, scaffoldBackgroundColor: Colors.white),
+//       home: Scaffold(
+//         body: Center(
+//           child: Text("Hello World")
+//         ),
+//       ),
+//     );
+//   }
+// }
